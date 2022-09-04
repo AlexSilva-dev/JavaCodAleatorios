9e94c78835f3b69e96e212863876d48841962cff
AlexSilva-dev <alexaleq321@gmail.com>
2022-09-04 02:37:49 -0300

    teste

---
+++ POO-UFF/Trabalho01-POO/src/Part2/OperarArraylist.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Part2;
+
+/**
+ *
+ * @author alexaleq321
+ */
+public class OperarArraylist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
