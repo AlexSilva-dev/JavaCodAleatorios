f682f0adc03ebe54bcd61efa9313b1ab840579e6
AlexSilva-dev <alexaleq321@gmail.com>
2022-09-30 23:17:26 -0300

    começo do trabalho de poo

---
+++ POO-UFF/Trabalho02-POO/src/Class/Funcionario.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author alex
+ */
+public class Funcionario {
+    // Atributos
+    private int matricula;
+    private String nome;
+    private float salario;
+    private float deducao;
+    
+    // Métodos
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public float getDeducao() {
+        return deducao;
+    }
+
+    public void setDeducao(float deducao) {
+        this.deducao = deducao;
+    }
+    
+    
+    
+}
---
+++ POO-UFF/Trabalho02-POO/src/trabalho02/poo/Trabalho02POO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalho02.poo;
+
+/**
+ *
+ * @author alex
+ */
+public class Trabalho02POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
